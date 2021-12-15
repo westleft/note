625505b25d95dd8e8cb3ecfb2424b788a4ecbd09
lin <leftwest576@gmail.com>
2021-12-15 22:42:22 +0800

    更改 css 樣式

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $currentNote = [
   'description' => ''
 ];
 
-if(isset($_GET['id'])){
+if (isset($_GET['id'])) {
   $currentNote = $connection->getNoteByID($_GET['id']);
 }
 
@@ -19,53 +19,60 @@ if(isset($_GET['id'])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>記事本 | CRUD練習</title>
-    <link rel="stylesheet" href="./scss/app.css">
-    <link rel="stylesheet" href="./scss/style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>記事本 | CRUD練習</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300;400&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./scss/main.css">
 </head>
+
 <body>
-<div>
+  <div class="main">
     <form class="new-note" action="./method/save.php" method="post">
-        <input type="hidden" name="id" value="<?php echo $currentNote['id'] ?>">
-        <input type="text" name="title" placeholder="Note title" autocomplete="off" value="<?php echo $currentNote['title'] ?>">
-        <textarea name="description" cols="30" rows="4"
-                  placeholder="Note Description"><?php echo $currentNote['description'] ?></textarea>
-        <button>
-          <?php if($currentNote['id']): ?>
-            Update Note
-          <?php else: ?>
-            New Note
-          <?php endif; ?>
-        </button>
+      <h2>優質便利貼</h2>
+      <input type="hidden" name="id" value="<?php echo $currentNote['id'] ?>">
+      <input type="text" name="title" placeholder="請輸入標題" autocomplete="off" value="<?php echo $currentNote['title'] ?>">
+      <textarea name="description" cols="30" rows="4" placeholder="請輸入內容"><?php echo $currentNote['description'] ?></textarea>
+      <button>
+        <?php if ($currentNote['id']) : ?>
+          更新內容
+        <?php else : ?>
+          新增內容
+        <?php endif; ?>
+      </button>
     </form>
     <div class="notes">
 
-        <?php foreach($notes as $note): ?>
+      <?php foreach ($notes as $note) : ?>
 
         <div class="note">
-            <div class="title">
-                <a href="?id=<?php echo $note['id']; ?>">
-                  <?php echo $note['title']; ?>
-                </a>
-            </div>
-            <div class="description">
-              <?php echo $note['description']; ?>
-            </div>
-            <small>
-              <?php echo $note['create_data']; ?>
-            </small>
-            <form action="./method/remove.php" method="post">
-              <input type="hidden" name="id" value="<?php echo $note['id']?>">
+          <div class="title">
+            <a href="?id=<?php echo $note['id']; ?>">
+              <?php echo $note['title']; ?>
+            </a>
+          </div>
+          <p class="date">
+            <?php echo $note['create_data']; ?>
+          </p>
+          <div class="description">
+            <?php echo $note['description']; ?>
+          </div>
+          
+          <form action="./method/remove.php" method="post">
+            <input type="hidden" name="id" value="<?php echo $note['id'] ?>">
             <button class="close">X</button>
-            </form>
+          </form>
         </div>
 
-        <?php endforeach; ?>
+      <?php endforeach; ?>
     </div>
-</div>
+  </div>
+
 </body>
+
 </html>
